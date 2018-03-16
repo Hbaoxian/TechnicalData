744cac1312997b20be984f0a754ddd47236b659f
黄保贤 <huangbaoxian@edaijia-inc.cn>
2018-03-16 19:48:51 +0800

    新增文件

---
+++ web/src/main/hbx.javanowledge.collect/loadFile/GitCommandController.java
@@ -0,0 +1,27 @@
+package loadFile;
+
+/**
+ * Created by huangbaoxian on 2018/3/16.
+ */
+public class GitCommandController {
+    /*
+    *
+    * 1 放弃上次提交的代码 远程
+    *
+    *
+    *   git revert HEAD
+        git push origin master
+
+
+
+      2  删除某一次提交
+
+        git rebase -i "commit id"^
+        git push origin master -f
+
+
+    *
+    *
+    * */
+
+}
--- web/src/main/hbx.javanowledge.collect/loadFile/LearningWebsite.java
+++ web/src/main/hbx.javanowledge.collect/loadFile/LearningWebsite.java
@@ -14,5 +14,6 @@ public class LearningWebsite {
     //https://segmentfault.com/a/1190000004716061
 
 
-
+    //滴滴 微服务重做之路
+    //https://mp.weixin.qq.com/s/5L-1EV_alM5EA7W0Av5gPw
 }
