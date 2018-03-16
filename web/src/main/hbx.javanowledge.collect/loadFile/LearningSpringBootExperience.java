15c8063cff494c2bc77074662c290d9bd0e5db79
黄保贤 <huangbaoxian@edaijia-inc.cn>
2018-03-16 20:05:25 +0800

    spring boot新建项目

---
+++ web/src/main/hbx.javanowledge.collect/loadFile/LearningSpringBootExperience.java
@@ -0,0 +1,53 @@
+package loadFile;
+
+/**
+ * Created by huangbaoxian on 2018/3/16.
+ */
+public class LearningSpringBootExperience {
+
+    /**
+     *
+     * 一   创建项目
+     *
+     * 2 选择spring initlalizer
+     *
+     * 3 选择web web
+     *
+     * 4 mvn clean install 下载依赖包
+     *
+     * 5 第一个helloworld控制器
+     *
+     * 5 mvn spring-boot:run 启动
+     *
+     *
+     *
+     *
+     *
+     * 二 ： jar包
+     *
+     * 查看是否存在依赖：
+     *
+     *
+     *
+     * <build>
+         <plugins>
+             <plugin>
+                 <groupId>org.springframework.boot</groupId>
+                <artifactId>spring-boot-maven-plugin</artifactId>
+            </plugin>
+        </plugins>
+     </build>
+
+     编译命令：mvn package
+
+     解压jar包： jar tvf target/myproject-0.0.1-SNAPSHOT.jar
+
+
+
+
+     *
+     *
+     * */
+
+
+}
