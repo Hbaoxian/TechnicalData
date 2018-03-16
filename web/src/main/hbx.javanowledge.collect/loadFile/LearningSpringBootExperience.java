c68fa1bd62f6af45135d04ccebef6f8eee1a5dc8
黄保贤 <huangbaoxian@edaijia-inc.cn>
2018-03-16 21:01:11 +0800

    新增文件

--- web/src/main/hbx.javanowledge.collect/loadFile/LearningSpringBootExperience.java
+++ web/src/main/hbx.javanowledge.collect/loadFile/LearningSpringBootExperience.java
@@ -40,7 +40,50 @@ public class LearningSpringBootExperience {
 
      编译命令：mvn package
 
-     解压jar包： jar tvf target/myproject-0.0.1-SNAPSHOT.jar
+     查看jar包内容： jar tvf   target/myproject-0.0.1-SNAPSHOT.jar
+
+     运行jar包： java -jar target/myproject-0.0.1-SNAPSHOT.jar
+
+
+
+
+
+
+     三： 非继承方式实现
+
+     <dependencyManagement>
+         <dependencies>
+            <!-- Override Spring Data release train provided by Spring Boot -->
+            <dependency>
+                <groupId>org.springframework.data</groupId>
+                 <artifactId>spring-data-releasetrain</artifactId>
+                 <version>Fowler-SR2</version>
+                 <type>pom</type>
+                 <scope>import</scope>
+        </dependency>
+        <dependency>
+                 <groupId>org.springframework.boot</groupId>
+                 <artifactId>spring-boot-dependencies</artifactId>
+                 <version>2.0.0.RELEASE</version>
+                 <type>pom</type>
+                 <scope>import</scope>
+         </dependency>
+      </dependencies>
+     </dependencyManagement>
+
+
+
+     maven插件：用来打包jar文件
+
+          <build>
+                 <plugins>
+                     <plugin>
+                         <groupId>org.springframework.boot</groupId>
+                         <artifactId>spring-boot-maven-plugin</artifactId>
+                     </plugin>
+                 </plugins>
+          </build>
+
 
 
 
